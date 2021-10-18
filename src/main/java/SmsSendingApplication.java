6f52b69eabdff1de728b9c9f6214c92f19a0af25
OKukotin <oleksandr.kukotin@gmail.com>
2021-10-19 00:19:37 +0300

    Created program from example by Twilio

--- src/main/java/SmsSendingApplication.java
+++ src/main/java/SmsSendingApplication.java
@@ -1,2 +1,17 @@
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
 public class SmsSendingApplication {
+
+    public static final String ACCOUNT_SID = System.getenv().getOrDefault("SID", "123");
+    public static final String AUTH_TOKEN = System.getenv().getOrDefault("TOKEN", "123");
+
+    public static void main(String[] args) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        Message message = Message.creator(new PhoneNumber("+380633660341"), new PhoneNumber("+16106325923"),
+                "Hello, Oleksandr!").create();
+        System.out.println(message.getSid());
+    }
 }
